d3cbb7e923cde0df0f54346a8b10e4f637a66a39
jeonghwan123 <dhshin444@naver.com>
2025-04-30 16:12:23 +0900

    고객 서버 연결 테스트

--- src/main/java/goorm/humandelivery/domain/model/response/CallRequestMessageResponse.java
+++ src/main/java/goorm/humandelivery/domain/model/response/CallRequestMessageResponse.java
@@ -1,10 +1,18 @@
 package goorm.humandelivery.domain.model.response;
 
+
 public class CallRequestMessageResponse {
 	private String message;
 	// 성공 실패 여부 Enum으로 추가
 
 	public CallRequestMessageResponse(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
 		this.message = message;
 	}
 
