804986ce570241c5adfdcf1ef8e36c5208cbee99
Duhikim <macksa29@hotmail.com>
2025-04-30 20:56:39 +0900

    [feat] 적절한 드라이버에게 메세지 전송
    전송 기능까지 구현하였음.
    택시 탐색 로직 업데이트 하여 연결하면 될듯
    
    Fixes:#35
    
    # Conflicts:
    #	src/main/java/goorm/humandelivery/application/WebSocketCustomerService.java
    #	src/main/java/goorm/humandelivery/domain/model/request/CustomerSocketMessageRequest.java

---
+++ src/main/java/goorm/humandelivery/domain/model/request/CustomerSocketMessageRequest.java
@@ -0,0 +1,26 @@
+package goorm.humandelivery.domain.model.request;
+
+import goorm.humandelivery.domain.model.entity.Location;
+import goorm.humandelivery.domain.model.entity.TaxiType;
+import goorm.humandelivery.domain.model.internal.CallMessage;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class CustomerSocketMessageRequest {
+
+	@NotBlank(message = "출발 위치를 입력해 주세요.")
+	private final Location expectedOrigin;
+	@NotBlank(message = "도착 위치를 입력해 주세요.")
+	private final Location expectedDestination;
+	@NotBlank(message = "택시 타입을 선택해 주세요.")
+	private final TaxiType taxiType;
+
+	public CallMessage toQueueMessage(Long CallId) {
+		return new CallMessage(CallId, expectedOrigin, expectedDestination, taxiType);
+	}
+
+}
